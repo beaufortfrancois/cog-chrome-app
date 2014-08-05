f861174ea012c3b88928bd4a4eca1cc2d22fb5d5
François Beaufort <beaufort.francois@gmail.com>
2014-08-05 16:21:10 +0200

    Removed useless comments in background.js

--- background.js
+++ background.js
@@ -1,14 +1,8 @@
-/**
- * Listens for the app launching then creates the window.
- *
- * @see http://developer.chrome.com/apps/app.runtime.html
- * @see http://developer.chrome.com/apps/app.window.html
- */
 chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create(
     "index.html",
     {
-      id: "mainWindow8",
+      id: "appWindow",
       innerBounds: {
         width: 960,
         height: 540,
