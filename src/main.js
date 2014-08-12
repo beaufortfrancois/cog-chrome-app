6fd1d98be9f2517fc5d9ba28abd27a8aaa091593
Francois Beaufort <beaufort.francois@gmail.com>
2014-08-12 15:30:24 +0200

    Fixed battery time formatting

--- src/main.js
+++ src/main.js
@@ -56,10 +56,12 @@ function updateBattery(batteryManager) {
 
     function formatSeconds(seconds) {
       if (seconds < 60) return seconds + ' s';
-      else if (seconds < 3600) return '00:' + (seconds / 60).toFixed(0);
-      else {
-        var hours = (seconds / 3600).toFixed(0);
-        var minutes = ((seconds - (hours * 3600)) / 60).toFixed(0);
+      else if (seconds < 3600) {
+        var minutes = Math.floor(seconds / 60).toFixed(0);
+        return '00:' + ((minutes > 9) ? minutes : '0' + minutes);
+      } else {
+        var hours = Math.floor(seconds / 3600).toFixed(0);
+        var minutes = Math.floor((seconds - (hours * 3600)) / 60).toFixed(0);
         return hours + ':' + ((minutes > 9) ? minutes : '0' + minutes);
       }
     };
