b4132e3898538f10a60d2e1a7eedd7c0ba411f4c
Francois Beaufort <beaufort.francois@gmail.com>
2015-02-27 09:19:57 +0100

    Added frame color for chrome 42

--- src/background.js
+++ src/background.js
@@ -3,6 +3,9 @@ chrome.app.runtime.onLaunched.addListener(function() {
     "index.html",
     {
       id: "appWindow",
+      frame: {
+        color: "#11575c"
+      },
       innerBounds: {
         width: 960,
         height: 540,
